8be86336c841a3508f98cc029b5ddd9e23a1a87b
Jannick Jungbluth <jannick2.0@googlemail.com>
2019-10-02 14:09:54 +0200

    added user feedback for todos

--- src/todo/todo.component.tsx
+++ src/todo/todo.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, } from '@uifabric/react-cards';
 import {
     Text,
@@ -9,29 +9,43 @@ import {
     FontSizes,
     DefaultPalette,
     MessageBar,
-    MessageBarType
+    MessageBarType,
+    ProgressIndicator
 } from 'office-ui-fabric-react';
 
 import ITodoItem from '../models/ITodoItem.model';
 import ITodoService from '../services/todo-service.interface';
 import TodoService from '../services/todo.service';
+import { AjaxState } from '../enums/ajax-state.enum';
 
 const Todo = (props: { item: ITodoItem, onUpdate: Function, onDelete: Function }) => {
+
     const todoService: ITodoService = new TodoService();
 
+    const [submissionStatus, setSubmissionStatus] = useState<AjaxState>(AjaxState.initial);
+
     const clickedComplete = () => {
+        setSubmissionStatus(AjaxState.pending);
         todoService.update(props.item.id, { ...props.item, isComplete: true, })
 
             .then((updatedTodo: ITodoItem) => {
+                // set initial because we already have a message bar for completed todos
+                setSubmissionStatus(AjaxState.initial);
+                
                 props.onUpdate(props.item.id);
             })
 
             .catch(error => {
                 console.error(`Failed to update!`);
+                setSubmissionStatus(AjaxState.error);
+                setTimeout(() => {
+                    setSubmissionStatus(AjaxState.initial);
+                }, 3000);
             });
     };
 
     const clickedDelete = () => {
+        setSubmissionStatus(AjaxState.pending);
         todoService.deleteById(props.item.id)
 
             .then(() => {
@@ -40,13 +54,17 @@ const Todo = (props: { item: ITodoItem, onUpdate: Function, onDelete: Function }
 
             .catch(error => {
                 console.error(`Failed to delete!`);
+                setSubmissionStatus(AjaxState.error);
+                setTimeout(() => {
+                    setSubmissionStatus(AjaxState.initial);
+                }, 3000);
             });
     };
 
     return (
         <div>
             <Card styles={{ root: { textAlign: 'initial' } }} tokens={{ padding: 5, childrenGap: 5, width: '100%', maxWidth: '-webkit-fill-available' }}>
-                
+
                 {/* HEADING */}
                 <Card.Section tokens={{ padding: 4 }}>
                     <Stack horizontal tokens={{ childrenGap: 12 }}>
@@ -74,9 +92,28 @@ const Todo = (props: { item: ITodoItem, onUpdate: Function, onDelete: Function }
                     </Card.Section>
                 }
 
-                {/* BUTTONS */}
+                {/* BUTTONS & MESSAGES*/}
                 <Card.Section tokens={{ padding: 4 }} horizontalAlign={"end"} horizontal styles={{ root: { borderTop: `solid 1px ${DefaultPalette.neutralQuaternary}` } }}>
-                    {props.item.isComplete &&
+
+                    {/* AJAX STATE */}
+                    {submissionStatus === AjaxState.pending &&
+                        <StackItem grow>
+                            <ProgressIndicator />
+                        </StackItem>
+                    }
+
+                    {/* AJAX STATE */}
+                    {submissionStatus === AjaxState.error &&
+                        <StackItem grow>
+                            <MessageBar messageBarType={MessageBarType.error}>
+                                Operation failed!
+                                </MessageBar>
+                        </StackItem>
+                    }
+
+                    {/* COMLETION INDICATOR */}
+                    {(props.item.isComplete &&
+                        submissionStatus === AjaxState.initial) &&
                         <StackItem grow>
                             <MessageBar messageBarType={MessageBarType.success}>
                                 Completed!
