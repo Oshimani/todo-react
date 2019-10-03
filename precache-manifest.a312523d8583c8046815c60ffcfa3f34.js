4cf04932a22130a5dd205a3bac18c060304d7e83
Jannick Jungbluth <jjungbluth@ict.ag>
2019-10-04 00:33:52 +0200

    Updates

---
+++ precache-manifest.a312523d8583c8046815c60ffcfa3f34.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6990895181d3bd57ca0ebc674db796c",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "203af35dd4a7ad86141f",
+    "url": "/todo-react/static/js/2.14dc0175.chunk.js"
+  },
+  {
+    "revision": "47d4a260042e5863604f",
+    "url": "/todo-react/static/js/main.d70c561b.chunk.js"
+  },
+  {
+    "revision": "3343a1bbfbcd3f2c8cbb",
+    "url": "/todo-react/static/js/runtime-main.92a6bdf0.js"
+  }
+]);
